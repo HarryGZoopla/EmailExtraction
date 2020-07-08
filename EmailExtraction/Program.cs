011a69e043f9c7c9429d8794feadd140df69c91a
Harry <harry.galliano@zoopla.co.uk>
2020-07-08 13:47:57 +0100

    first commit

---
+++ EmailExtraction/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace EmailExtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String text = File.ReadAllText("sample.txt");
+
+            Regex rx = new Regex(@"@softwire\.com",
+            RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            MatchCollection matches = rx.Matches(text);
+
+            Console.WriteLine(matches.Count);
+
+
+
+
+        }
+    }
+}
